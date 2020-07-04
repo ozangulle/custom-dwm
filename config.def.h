19dab295840ddfeb31c21d507f12cbd7536c0b52
Ozan Gulle <ozan@ozan-laptop.fritz.box>
2020-07-04 22:17:27 +0200

    Added shortcut for lock screen

--- config.def.h
+++ config.def.h
@@ -77,6 +77,7 @@ static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "5%-", "unm
 // static const char *miccmd[] = { "amixer", "set", "Capture", "toggle", NULL };
 static const char *brupcmd[] = { "sudo", "xbacklight", "-inc", "10", NULL };
 static const char *brdowncmd[] = { "sudo", "xbacklight", "-dec", "10", NULL };
+static const char *lockcmd[] = {"slock", NULL};
 
 #include "movestack.c"
 static Key keys[] = {
@@ -111,6 +112,7 @@ static Key keys[] = {
 	{ 0, 				XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
 	{ 0, 				XF86XK_MonBrightnessUp, spawn, {.v = brupcmd} },
 	{ 0, 				XF86XK_MonBrightnessDown, spawn, {.v = brdowncmd} },
+	{ Mod4Mask,			XK_l,	   spawn,	   {.v = lockcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
